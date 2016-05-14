b7d8b62cf038cccc04c31a8bf912ae717f91a894
Qin Cheng <pqc92@hotmail.com>
2016-05-15 02:21:53 +0800

    refactored pdf creation in server

---
+++ PDFService.js
@@ -0,0 +1,25 @@
+var path =  require('path');
+var marked = require('marked');
+var handlebars = require('handlebars');
+var fs = require('fs');
+var pdf = require('html-pdf');
+
+var source = fs.readFileSync('./template.html', 'utf8');
+var template = handlebars.compile(source);
+var options = {
+  format: "A4",
+  base: path.join('file:///',__dirname, 'app/base.css'),
+  border: "30px"
+};
+
+module.exports = {
+  run: function(input) {
+    var html = marked(input, {sanitize: true, gfm: true, breaks: true});
+    var data = {content: html};
+    var result = template(data);
+    pdf.create(result, options).toFile('./out/test2.pdf', function(err, res) {
+      if (err) return console.log(err);
+      console.log(res);
+    });
+  }
+};
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var path =  require('path');
-var marked = require('marked');
 var bodyParser = require('body-parser');
-var handlebars = require('handlebars');
-var fs = require('fs');
-var pdf = require('html-pdf');
+var PDFService = require('./PDFService');
 var app = express();
 
 
@@ -12,23 +9,9 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
-
-var source = fs.readFileSync('./template.html', 'utf8')
-var template = handlebars.compile(source)
-var options = {
-  format: "A4",
-  base: "file:///Users/pehqincheng/Desktop/markdowntgt/app/base.css",
-  border: "30px"
-}
-
 app.post('/send', function (req, res) {
-  var html = marked(req.body.text, {sanitize: true, gfm: true, breaks: true});
-  var data = {content: html};
-  var result = template(data);
-  pdf.create(result, options).toFile('./out/test2.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res);
-  });
+  //handle req.body.styles
+  PDFService.run(req.body.text);
   return res.send({status: 'OK'});
 });
 
