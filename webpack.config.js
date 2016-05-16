3ddc6764b20621fcaffaf90388bf77cd81ce8c67
Qin Cheng <pqc92@hotmail.com>
2016-05-17 00:43:14 +0800

    fixed path issue for windows

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
+const path = require('path');
+
 module.exports = {
-  context: __dirname + "/app",
-  entry: {
-    javascript: "./app.js",
-    html: "./index.html"
-  },
+ context: path.join(__dirname, "app"),
+ entry: {
+   javascript: "./app.js",
+   html: "./index.html"
+ },
 
-  output: {
-    filename: "app.js",
-    path: __dirname + "/dist",
-  },
+ output: {
+   filename: "app.js",
+   path: path.join(__dirname, "dist"),
+ },
 
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query:
-          {
-            presets:['react', 'es2015']
-          }
-      },
+ module: {
+   loaders: [
+     {
+       test: /\.js$/,
+       exclude: /node_modules/,
+       loader: "babel-loader",
+       query:
+         {
+           presets:['react', 'es2015']
+         }
+     },
 
-      {
-        test: /\.html$/,
-        loader: "file?name=[name].[ext]",
-      }
-    ],
-  },
+     {
+       test: /\.html$/,
+       loader: "file?name=[name].[ext]",
+     }
+   ],
+ },
+
 }
